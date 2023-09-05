2668f4ab44e2566de1856de1d0e4d7e2f6360611
AlexT117 <tellezromeroalexis@gmai.com>
2023-09-05 13:12:25 -0600

    Archivo de conexión

---
+++ conexion.php
@@ -0,0 +1,9 @@
+<?php
+$server = "localhost";
+$user = "root";
+$pass = "12345678";
+$bd = "agencia";
+$conexion = new mysqli($server, $user, $pass, $bd);
+
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
-    echo "Proyecto global";
+    require("conexion.php");
+    echo "Proyecto global <br>";
     
+    $query = "SELECT * FROM vehiculo";
+
+    if ($result = $conexion->query($query)) {
+		while ($row = $result->fetch_assoc()) {
+			$idV = $row["idVehiculo"];
+			$idM = $row["idMarca"];
+            $pre = $row["precio"];
+            $mot = $row["motor"];
+            $idT = $row["idTraccion"];
+            $Nom = $row["nombre"];
+            $idTDV = $row["idTipoDeVehiculo"];
+            $sta = $row["status"];
+			echo $idV . " " . $idM . " " . $pre . " " . $mot . " " . 
+            $idT . " " . $Nom . " " . $idTDV . " " . $sta . " " . "<br>";
+		}
+
+	/*freeresultset*/
+	$result->free();
+    }
 ?>
